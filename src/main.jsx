1244911f8d9c4631f90a8aee7bb80e5255400406
finallycecilio <cecilioficial@gmail.com>
2026-02-16 14:29:52 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MobileShop from './MobileShop.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MobileShop />
-  </React.StrictMode>,
-)
